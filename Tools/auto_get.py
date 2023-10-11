7d6edb548d5e2165a1d374a3089037e58215783b
ZoldicintheShell <123384683+ZoldicintheShell@users.noreply.github.com>
2023-10-11 23:16:21 +0200

    Update auto_get.py

--- Tools/auto_get.py
+++ Tools/auto_get.py
@@ -22,6 +22,29 @@ print("The downloaded folder is located at:", data_dir)
 
 #-------------------- DATA FOLDERS ARCHITECTURE
 
+# Chemin vers le dossier "datasets"
+datasets_dir = "datasets/PetImages"
+
+# Parcourir les dossiers dans "datasets"
+for dossier in os.listdir(datasets_dir):
+    dossier_complet = os.path.join(datasets_dir, dossier)
+    
+    # Vérifier si l'élément est un dossier
+    if os.path.isdir(dossier_complet):
+        # Parcourir les fichiers dans le dossier
+        for fichier in os.listdir(dossier_complet):
+            chemin_source = os.path.join(dossier_complet, fichier)
+            
+            # Vérifier si l'élément est un fichier
+            if os.path.isfile(chemin_source):
+                # Créer le nouveau nom en ajoutant le nom du dossier en préfixe
+                nouveau_nom = f"{dossier}_{fichier}"
+                chemin_destination = os.path.join(dossier_complet, nouveau_nom)
+                
+                # Renommer le fichier
+                os.rename(chemin_source, chemin_destination)
+                print(f"Renommage de {fichier} en {nouveau_nom}")
+
 # Rename the "datasets" folder to "initial_dataset"
 if os.path.exists("datasets"):
     os.rename("datasets", "initial_dataset")
@@ -45,3 +68,6 @@ for filename in os.listdir(dog_source_dir):
         shutil.copy(os.path.join(dog_source_dir, filename), os.path.join(train_dir, filename))
 
 print("Operation completed: The 'initial_dataset' folder has been renamed, and the files have been copied to 'Dataset/train'.")
+
+# move "test_set" into "Dataset" folder
+shutil.move('Test_set', 'Dataset')
